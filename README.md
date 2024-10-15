bd2b9d45b22cc0d1db1fb82c2ae91e97d8795107
iamhvsharma <itshvsharma@gmail.com>
2024-10-15 14:49:35 +0530

    Request Airdrop v1

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Token Launchpad 
 
 Steps while buidling:- 
+* Install Wallet Adapter Dependencies
+
+``` npm install @solana/wallet-adapter-base @solana/wallet-adapter-react @solana/wallet-adapter-react-ui @solana/wallet-adapter-wallets @solana/web3.js ```
 
 * 
--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { FC, useMemo } from 'react';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
+import {
+    WalletModalProvider,
+    WalletDisconnectButton,
+    WalletMultiButton
+} from '@solana/wallet-adapter-react-ui';
+import { clusterApiUrl } from '@solana/web3.js';
+
+// Default styles that can be overridden by your app
+import '@solana/wallet-adapter-react-ui/styles.css';
+
 import './App.css'
+import { RequestAirdrop } from './RequestAirdrop';
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-   <div>
-    <h1>Hi there</h1>
-   </div>
+    <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
+    <WalletProvider wallets={[]} autoConnect>
+        <WalletModalProvider>
+          <div>
+            <WalletMultiButton />
+            <RequestAirdrop />
+          </div>
+        </WalletModalProvider>
+    </WalletProvider>
+</ConnectionProvider>
   )
 }
 
